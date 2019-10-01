d022e9d82533b6b68a34a8152eb891491492bceb
Zakria Hussine <zakria.hussine@gmail.com>
2019-10-01 14:42:00 -0600

    completing the function for c1m1 Assessment

--- stats.c
+++ stats.c
@@ -9,35 +9,28 @@
  *
  *****************************************************************************/
 /**
- * @file <stats.c>
- * @brief < first assignment main function and the other functions ,that doing some math work >
+ * @file stats.c
+ * @brief first assignment main function and the other functions ,that doing some math work >
  *
- * <main function and other functions to print a array and find the max,min,mean and the median for an array
+ *         main function and other functions to print a array and find the max,min,mean and the median for an array
 
  >
  *
- * @author <Zakria Hussine>
- * @date <30.09.2019>
+ * @author Zakria Hussine
+ * @date 30.09.2019
  *
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+//#include "stats.h"
 
-int main()
-{
-#include <stdio.h>
-#include "stats.h"
-
-void print_statistics(unsigned char * ptr, int count);
+unsigned char print_statistics(unsigned char * ptr, int count);
 void print_array(unsigned char * ptr, int count);
 unsigned char find_median(unsigned char * ptr, int count);
 unsigned char find_mean(unsigned char * ptr, int count);
 unsigned char find_maximum(unsigned char * ptr, int count);
 unsigned char find_minimum(unsigned char * ptr, int count);
 unsigned char sort_array(unsigned char * ptr, int count);
-
-
 
 /* Size of the Data Set */
 #define SIZE (40)
@@ -53,26 +46,164 @@ void main() {
   /* Other Variable Declarations Go Here */
 
   /* Statistics and Printing Functions Go Here */
-    print_array(test,SIZE);
+
     print_statistics(test,SIZE);
+    print_array(test,SIZE);
+    printf("\n");
 
+
+};
+
+unsigned char print_statistics(unsigned char * ptr, int count){
+
+
+unsigned char median = find_median(ptr,count) ;
+
+unsigned char mean = find_mean(ptr,count) ;
+
+unsigned char max = find_maximum(ptr,count) ;
+
+unsigned char min = find_minimum(ptr,count) ;
+
+
+printf("the mean  is : %u \n",mean);
+printf("the median number is : %u \n",median);
+printf("the max number  is : %u \n",max);
+printf("the min number  is : %u \n",min);
 }
-void print_statistics(unsigned char * ptr, int count){
-unsigned char median = find_median(* ptr,count) ;
-unsigned char mean = find_mean(* ptr,count) ;
-unsigned char max = find_maximum(* ptr,count) ;
-unsigned char median = find_minimum(* ptr,count) ;
 
-}
+void print_array(unsigned char * ptr, int count){
+    int i,n;
+    n=0;
+    for(i=0;i<count;i++)
+        {
+        printf(" %u  ",ptr[i]);
+        n++;
+        if(n>7)
+        {
+            n=0;
+            printf("\n");
+        }
+        }
 
-void print_array(unsigned char * ptr, int count){};
-unsigned char find_median(unsigned char * ptr, int count){};
-unsigned char find_mean(unsigned char * ptr, int count){};
-unsigned char find_maximum(unsigned char * ptr, int count){};
-unsigned char find_minimum(unsigned char * ptr, int count){};
-unsigned char sort_array(unsigned char * ptr, int count){};
-
-/* Add other Implementation File Code Here */
+};
+unsigned char sort_array(unsigned char * ptr, int count){
+int temp,i,j;
+if(ptr==0){
     return 0;
 }
+if (count <= 0 ){
+    return 0;
+}
+unsigned char sorted[count];
+for(i=0;i<count;i++)
+{
+    sorted[i]=ptr[i];
+
+}
+for(i=1;i<count;i++)
+{
+     for(j=0;j<count-i;j++)
+     {
+         if(sorted[j]<sorted[j+1])
+         {
+             temp=sorted[j];
+             sorted[j]=sorted[j+1];
+             sorted[j+1]=temp;
+         }
+     }
+}
+print_array(sorted,count);
+ return sorted;
+
+}
+unsigned char find_mean(unsigned char * ptr, int count){
+int avg , i ;
+
+if(ptr==0){
+    return 0;
+}
+if (count <= 0 ){
+    return 0;
+}
+
+for(i=0;i<count;i++)
+{
+    avg += ptr[i];
+
+}
+return (avg/count);
+};
+
+
+unsigned char find_median(unsigned char * ptr, int count){
+
+
+if(ptr==0){
+    return 0;
+}
+if (count <= 0 ){
+    return 0;
+}
+
+
+ if((count%2)==0){
+    sort_array(ptr,count);
+    return (ptr[(count-1)/2] + ptr[count/2])/2;
+ }else
+ {
+     return ptr[count/2];
+ }
+
+};
+
+
+
+
+unsigned char find_maximum(unsigned char * ptr, int count){
+
+
+if(ptr==0){
+    return 0;
+}
+if (count <= 0 ){
+    return 0;
+}
+int max,i;
+max=ptr[0];
+for(i=1;i<count;i++)
+{
+    if(max<ptr[i])
+        max=ptr[i];
+
+}
+
+return max;
+
+};
+unsigned char find_minimum(unsigned char * ptr, int count){
+
+
+unsigned char array_min[count];
+if(ptr==0){
+    return 0;
+}
+if (count <= 0 ){
+    return 0;
+}
+int min,i;
+min=ptr[0];
+for(i=1;i<count;i++)
+{
+    if(min>ptr[i])
+        min=ptr[i];
+
+}
+return min;
+
+};
+
+/* Add other Implementation File Code Here */
+
+
 
